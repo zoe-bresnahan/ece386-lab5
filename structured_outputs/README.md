21f40acaf1d6008fe8a1ff7a0e8bb14693175e46
byarbrough <6315292+byarbrough@users.noreply.github.com>
2025-04-04 10:50:52 -0600

    Add strucutred outputs

---
+++ structured_outputs/README.md
@@ -0,0 +1,9 @@
+# Structured Outputs
+
+***Student:** Complete below.*
+
+## How well did it work?
+
+## Paste a few input/output combos
+
+*use code blocks for the JSON*
---
+++ structured_outputs/user_creation.py
@@ -0,0 +1,5 @@
+'''Passes a personal intro statement to an LLM.
+The LLM produces valid JSON that could be ingested into a database to create a new user.
+
+Modified from https://ollama.com/blog/structured-outputs
+'''
